2112a50ca1cf63f42dab2e62d8e22b7c979fde5b
Hadley Pope <hpope@umass.edu>
2020-05-03 13:36:26 -0400

    Updated to take types from frontend instead of ids. Added update to supporter type

--- lambdas/update_supporter_settings.py
+++ lambdas/update_supporter_settings.py
@@ -11,7 +11,8 @@ import json
 #        hours_before_appointment, grad_student,
 #        specialization_type_id, max_students, duration,
 #        list of link_id/link pairs, notification_preferences,
-#        list of tag_type_ids, list of major_ids, list of minor_ids,         
+#        list of tag_type_ids, list of major_ids, list of minor_ids,
+#        list of supporter types         
 #Output: 200 OK
 
 def update_supporter_settings(event, context):
@@ -26,89 +27,98 @@ def update_supporter_settings(event, context):
         raise LambdaException("404: Supporter does not exist")
 
 
-    link_list = event['links']
-
-    #Check if link_id exists
-    for link_id, link in link_list:
-        sql = 'SELECT * FROM link WHERE link_id = :link_id;'
-        sql_parameters = [{'name': 'link_id', 'value': {'longValue': entry}}]
+    link_type_list = event['links']
+    link_id_list = []
+    #Check if link_type exists, and convert to link_id
+    for link_type, link in link_type_list:
+        sql = 'SELECT link_id FROM link WHERE link_type = :link_type;'
+        sql_parameters = [{'name': 'link_type', 'value': {'longValue': link_type}}]
         response = query(sql, sql_parameters)
         if(response['records'] ==[]):
             raise LambdaException("404: link_id:" + str(entry) + " does not exist")
+        else:
+            link_id_list.append(response['records'][0][0]['longValue'])
 
 
-    tag_type_id_list = event['tags']
-
-    #Check if tag_type_id exists
-    for entry in tag_type_id_list:
-        sql = 'SELECT * FROM tag_type WHERE tag_type_id = :tag_type_id;'
-        sql_parameters = [{'name': 'tag_type_id', 'value': {'longValue': entry}}]
+    tag_type_list = event['tags']
+    tag_type_id_list = []
+    #Check if tag_type exists, and convert to tag_type_id
+    for entry in tag_type_list:
+        sql = 'SELECT tag_type_id FROM tag_type WHERE tag_type = :tag_type;'
+        sql_parameters = [{'name': 'tag_type', 'value': {'longValue': entry}}]
         response = query(sql, sql_parameters)
         if(response['records'] ==[]):
             raise LambdaException("404: tag_type_id:" + str(entry) + " does not exist")
+        else:
+            tag_type_id_list.append(response['records'][0][0]['longValue'])
 
 
-    major_id_list = event['major_id']
-
+    major_list = event['major_id']
+    major_id_list = []
     #Check if major_id exists
-    for entry in major_id_list:
-        sql = 'SELECT * FROM major WHERE major_id = :major_id;'
-        sql_parameters = [{'name': 'major_id', 'value': {'longValue': entry}}]
+    for entry in major_list:
+        sql = 'SELECT major_id FROM major WHERE major = :major;'
+        sql_parameters = [{'name': 'major', 'value': {'longValue': entry}}]
         response = query(sql, sql_parameters)
         if(response['records'] ==[]):
             raise LambdaException("404: major_id:" + str(entry) + " does not exist")
+        else:
+            major_id_list.append(response['records'][0][0]['longValue'])
 
     
     #Check if minor_id exists
-    minor_id_list = event['minor_id']
-
-    for entry in minor_id_list:
-        sql = 'SELECT * FROM minor WHERE minor_id = :minor_id;'
-        sql_parameters = [{'name': 'minor_id', 'value': {'longValue': entry}}]
+    minor_list = event['minor_id']
+    minor_id_list = []
+    for entry in minor_list:
+        sql = 'SELECT minor_id FROM minor WHERE minor = :minor;'
+        sql_parameters = [{'name': 'minor', 'value': {'longValue': entry}}]
         response = query(sql, sql_parameters)
         if(response['records'] ==[]):
             raise LambdaException("404: minor_id:" + str(entry) + " does not exist")
+        else:
+            minor_id_list.append(response['records'][0][0]['longValue'])
+
 
     #User settings
     user_settings = {}
-    updated_user_vals = ""
+    updated_user_vals = []
 
     if event['first_name'] != "":
         first_name = event['first_name']
         user_settings['first_name'] = first_name
-        updated_user_vals += " first_name = :first_name, "
+        updated_user_vals.append("first_name = :first_name")
 
     if event['last_name'] != "":        
         last_name = event['last_name']
         user_settings['last_name'] = last_name
-        updated_user_vals += " last_name = :last_name, "
+        updated_user_vals.append("last_name = :last_name")
 
     if event['preferred_name'] != "":
         preferred_name = event['preferred_name']
         user_settings['preferred_name'] = preferred_name
-        updated_user_vals += " preferred_name = :preferred_name, "
+        updated_user_vals.append("preferred_name = :preferred_name")
 
     if event['picture'] != "":
         picture = event['picture']
         user_settings['picture'] = picture
-        updated_user_vals += " picture = :picture, "
+        updated_user_vals.append("picture = :picture")
 
     if event['bio'] != "":
         bio = event['bio']
         user_settings['bio'] = bio
-        updated_user_vals += " bio = :bio,"
+        updated_user_vals.append("bio = :bio")
 
     if event['pronouns'] ! = "":
         pronouns = event['pronouns']
         user_settings['pronouns'] = pronouns
-        updated_user_vals += " pronouns = :pronouns,"
+        updated_user_vals.append("pronouns = :pronouns")
 
     if event['phone'] != "":
         phone = event['phone']
         user_settings['phone'] = phone
-        updated_user_vals += " phone = :phone,"
+        updated_user_vals.append("phone = :phone")
 
-    user_settings_sql = "UPDATE users SET" + updated_user_vals[:-1] + " WHERE user_id = :supporter_id;"
+    user_settings_sql = "UPDATE users SET" + ", ".join(updated_user_vals) + " WHERE user_id = :supporter_id;"
     user_settings_params = dictionary_to_list(user_settings) 
     user_settings_params.append(supporter_id_param)
 
@@ -121,29 +131,29 @@ def update_supporter_settings(event, context):
 
     #Supporter Settings
     supporter_settings = {}
-    updated_supporter_vals = ""
+    updated_supporter_vals = []
 
     if event['employer'] != "":
         employer = event['employer']
         supporter_settings['employer'] = employer
-        updated_supporter_vals += " employer = :employer,"
+        updated_supporter_vals.append("employer = :employer")
 
     if event['title'] != "":
         title = event['title']
         supporter_settings['title'] = title
-        updated_supporter_vals += " title = :title,"
+        updated_supporter_vals.append("title = :title")
 
     if event['team_name'] != "":
         team_name = event['team_name']
         supporter_settings['team_name'] = team_name
-        updated_supporter_vals += " team_name = :team_name"
+        updated_supporter_vals.append("team_name = :team_name")
 
     if event['office'] != "":
         office = event['office']
         supporter_settings['office'] = office
-        updated_supporter_vals += " office = :office"
+        updated_supporter_vals.append("office = :office")
 
-    supporter_settings_sql = "UPDATE supporters SET" + updated_supporter_vals[:-1] + " WHERE supporter_id = :supporter_id;"
+    supporter_settings_sql = "UPDATE supporters SET" + ", ".join(updated_supporter_vals) + " WHERE supporter_id = :supporter_id;"
     supporter_settings_params = dictionary_to_list(supporter_settings)
     supporter_settings_params.append(supporter_id_param)
 
@@ -156,24 +166,24 @@ def update_supporter_settings(event, context):
 
     #Supporter Specializations
     supporter_specialization = {}
-    updated_specializations = ""
+    updated_specializations = []
 
     if event['specialization_type_id'] != "":
         specialization_type_id = event['specialization_type_id']
         supporter_specialization['specialization_type_id'] = specialization_type_id
-        updated_specializations += " specialization_type_id = :specialization_type_id,"
+        updated_specializations.append("specialization_type_id = :specialization_type_id")
 
     if event['max_students'] != "":
         max_students = event['max_students']
         supporter_specialization['max_students'] = max_students
-        updated_specializations += " max_students = :max_students,"
+        updated_specializations.append("max_students = :max_students")
 
     if event['duration'] != "":
         duration = event['duration']
         supporter_specialization['duration'] = duration
-        updated_specializations += " duration = :duration,"
+        updated_specializations.append("duration = :duration")
 
-    specializations_sql = "UPDATE supporter_specializations SET" + updated_specializations[:-1] + " WHERE supporter_id = :supporter_id;"
+    specializations_sql = "UPDATE supporter_specializations SET" + ", ".join(updated_specializations) + " WHERE supporter_id = :supporter_id;"
     specializations_params = dictionary_to_list(supporter_specialization)
     specializations_params.append(supporter_id_param)
 
@@ -186,19 +196,19 @@ def update_supporter_settings(event, context):
 
     #Supporter Preferences for Students
     supporter_student_prefs = {}
-    updated_student_prefs = ""
+    updated_student_prefs = []
 
     if event['grad_student'] != "":
         grad_student = event['grad_student']
         supporter_student_prefs['grad_student'] = grad_student
-        updated_student_prefs += " grad_student = :grad_student,"
+        updated_student_prefs.append("grad_student = :grad_student")
 
     if event['hours_before_appointment'] != "":
         hours_before_appointment = event['hours_before_appointment']
         supporter_student_prefs['hours_before_appointment'] = hours_before_appointment
-        updated_student_prefs += " hours_before_appointment = :hours_before_appointment,"
+        updated_student_prefs.append("hours_before_appointment = :hours_before_appointment")
 
-    student_prefs_sql = "UPDATE supporter_preferences_for_students SET" + updated_student_prefs[:-1] + " WHERE supporter_id = :supporter_id;"
+    student_prefs_sql = "UPDATE supporter_preferences_for_students SET" + ", ".join(updated_student_prefs) + " WHERE supporter_id = :supporter_id;"
     student_prefs_params = dictionary_to_list(supporter_student_prefs)
     student_prefs_params.append(supporter_id_param)
 
@@ -218,7 +228,7 @@ def update_supporter_settings(event, context):
         raise LambdaException("500: Unable to delete links")
 
     #Execute parameterized queries for updating links
-    for link_id, link in link_list:
+    for link_id, link in link_id_list:
         sql = 'INSERT INTO user_links(user_id, link_id, link) VALUES (:user_id, :link_id, :link);'
         sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}, {'name': 'link_id', 'value': {'longValue': link_id}}, {'name': 'link', 'value': {'longValue': link}}]
         major_response = query(sql, sql_parameters)
@@ -280,8 +290,36 @@ def update_supporter_settings(event, context):
         minor_response = query(sql, sql_parameters)
         if(minor_response["numberOfRecordsUpdated"] == 0):
             raise LambdaException("409: supporter_minorr_preferences minors not updated")
+
+
+    #Update supporter types
+    if event['supporter_types'] != []:
+
+        supporter_types = event['supporter_types']
+
+        updated_supporter_types = []
+        if "Professional Staff" in supporter_types:
+            updated_supporter_types.append("professional_staff = true")
+        
+        if "Student Staff" in supporter_types:
+            updated_supporter_types.append("student_staff = true")
+
+        if "Alumni" in supporter_types:
+            updated_supporter_types.append("alumni = true")
+        
+        if "Faculty" in supporter_types:
+            updated_supporter_types.append("faculty = true")
+
+        if "Other" in supporter_types:
+            updated_supporter_types.append("other = true")
+
+        supporter_types_sql = "UPDATE supporter_type SET " + ", ".join(updated_supporter_types) + " WHERE supporter_id = :supporter_id;"
+        try:
+            query(supporter_types_sql, supporter_id_param)
+        except Exception as e:
+            raise LambdaException("500: Unable to update supporter_types, " + str(e))
             
     
     return {
-        'statusCode': 200
+        'body': "Successfully updated supporter settings"
     }
